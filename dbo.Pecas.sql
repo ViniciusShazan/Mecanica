2317681ad7f2819e607f076f13c5e3156669ea4d
ViniciusShazan <vinima30@gmail.com>
2017-06-29 16:31:38 -0300

    Add files via upload

---
+++ dbo.Pecas.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Pecas] (
+    [id]         INT          IDENTITY (1, 1) NOT NULL,
+    [nome]       VARCHAR (25) NULL,
+    [marca]      VARCHAR (15) NULL,
+    [modelo]     VARCHAR (20) NULL,
+    [preço]      INT          NULL,
+    [quantidade] INT          NULL,
+    [descricao]  VARCHAR (50) NULL,
+    [servico_id] INT          NULL,
+    CONSTRAINT [PK_Pecas] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
